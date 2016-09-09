bf7586564938c769b5bc0203d8fe0524e38d849b
John Stockbauer <john@shippable.com>
2016-09-08 17:23:09 -0700

    incorrect integration label names

--- pushRelease.sh
+++ pushRelease.sh
@@ -30,8 +30,8 @@ configure_aws() {
   echo "Extracting ECR credentials"
   . $creds_path
   echo "Configuring aws cli with ECR credentials"
-  aws configure set aws_access_key_id $awsAccessKeyId
-  aws configure set aws_secret_access_key $awsSecretAccessKey
+  aws configure set aws_access_key_id $aws_access_key_id
+  aws configure set aws_secret_access_key $aws_secret_access_key
   aws configure set region $PUSH_REGION
   echo "Successfully configured aws cli credentials"
 }
