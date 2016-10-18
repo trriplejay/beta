a7cfa8f02aa9267f0800957c090a919bd5acff9a
Devashish <devashish.86@gmail.com>
2016-10-18 16:02:16 -0700

    adding node variables

--- deployAlpha.sh
+++ deployAlpha.sh
@@ -5,10 +5,11 @@ export RES_RELEASE=rel-alpha-server
 #export ALPHA_INTEGRATION=aws-alpha-pem
 export ALPHA_INTEGRATION=shippable-alpha-pem
 export ALPHA_SWARM=aws-alpha-swarm
-export ALPHA_SWARM_BASTION_USER=""
-export ALPHA_SWARM_BASTION_IP=""
+export ALPHA_BASTION_USER=""
+export ALPHA_BASTION_IP=""
 export ALPHA_SWARM_USER=""
 export ALPHA_SWARM_IP=""
+export KEY_FILE_PATH=""
 
 parse_version() {
   release_path="IN/$RES_RELEASE/release/release.json"
@@ -26,31 +27,33 @@ load_node_info() {
   echo "Loading node information"
   local node_info=$(cat IN/$ALPHA_SWARM/params)
   export $node_info
+  . $node_info
 
   echo "########### USER: $ALPHA_SWARM_USER"
   echo "########### IP_ADDR: $ALPHA_SWARM_IP"
+  echo "########### BASTION USER: $ALPHA_BASTION_USER"
+  echo "########### BASTION IP_ADDR: $ALPHA_BASTION_IP"
   echo "successfully loaded node information"
 }
 
 configure_node_creds() {
+  echo "Extracting AWS PEM"
+  echo "-----------------------------------"
   local creds_path="IN/$ALPHA_INTEGRATION/integration.env"
   if [ ! -f $creds_path ]; then
     echo "No credentials file found at location: $creds_path"
     return 1
   fi
 
-  echo "Extracting node credentials"
-  echo "++++++++++++++++++++++++++++++++++"
-  cat $creds_path
-  . $creds_path
-  echo "configuring node credentials"
-  local write_key=$(echo $key | tee IN/$ALPHA_INTEGRATION/key.pem)
-  echo $write_key
-  echo '****************************'
-  cat $creds_path
-  local update_mode=$(chmod -cR 600 IN/$ALPHA_INTEGRATION/key.pem)
-  echo $update_mode
-  #ssh-add IN/$ALPHA_INTEGRATION/key.pem
+  export KEY_FILE_PATH="IN/$ALPHA_INTEGRATION/key.pem"
+  cat IN/$ALPHA_INTEGRATION/integration.json  \
+    | jq -r '.key' > $KEY_FILE_PATH
+  chmod 600 $KEY_FILE_PATH
+
+  echo "Completed Extracting AWS PEM"
+  echo "-----------------------------------"
+
+  ssh-add $KEY_FILE_PATH
 }
 
 deploy() {
@@ -62,7 +65,7 @@ deploy() {
 }
 
 main() {
-  eval $(ssh-agent)
+  eval $(ssh-agent -s)
   manifest_path="IN/$RES_RELEASE/release/manifests.json"
   if [ ! -e $manifest_path ]; then
     echo "No manifests.json file found at location: $manifest_path"
