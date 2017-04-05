e48ed28a5139a87f97eea388120c6d64ede80db7
chetantarale <chetan_tarale@yahoo.com>
2017-04-05 15:47:28 +0530

    build docs

--- deployDocs.sh
+++ deployDocs.sh
@@ -7,15 +7,11 @@ export AWS_S3_LOCAL_PATH="site"
 sync_docs() {
   pushd IN/docs_repo/gitRepo/
 
-  mkdir -p "$AWS_S3_LOCAL_PATH"
-  cp -r sources "$AWS_S3_LOCAL_PATH"
-  cp -r themes "$AWS_S3_LOCAL_PATH"
-  #echo "Installing requirements with pip"
-  # pip install -r requirements.txt
+  echo "Installing requirements with pip"
+  pip install -r requirements.txt
 
-  # echo "Building docs"
-  # mkdocs build
-
+  echo "Building docs"
+  mkdocs build
 
   echo "Syncing with S3"
   aws s3 sync $AWS_S3_LOCAL_PATH $DOCS_BUCKET --delete --acl public-read --region $DOCS_REGION
