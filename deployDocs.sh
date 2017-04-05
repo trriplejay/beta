9d392e3583aba7e3a1736f287850a0479b745ada
chetantarale <chetan_tarale@yahoo.com>
2017-04-05 14:02:21 +0530

    remove directory checks

--- deployDocs.sh
+++ deployDocs.sh
@@ -7,18 +7,15 @@ export AWS_S3_LOCAL_PATH="site"
 sync_docs() {
   pushd IN/docs_repo/gitRepo/
 
-  if [ -d "$AWS_S3_LOCAL_PATH" ]; then
-    cp -r sources "$AWS_S3_LOCAL_PATH"
-    cp -r themes "$AWS_S3_LOCAL_PATH"
-    # echo "Installing requirements with pip"
-    # pip install -r requirements.txt
+  mkdir -p "$AWS_S3_LOCAL_PATH"
+  cp -r sources "$AWS_S3_LOCAL_PATH"
+  cp -r themes "$AWS_S3_LOCAL_PATH"
+  #echo "Installing requirements with pip"
+  # pip install -r requirements.txt
 
-    # echo "Building docs"
-    # mkdocs build
-  else
-    mkdir -p "$AWS_S3_LOCAL_PATH"
-    echo "Directory created: $AWS_S3_LOCAL_PATH"
-  fi
+  # echo "Building docs"
+  # mkdocs build
+
 
   echo "Syncing with S3"
   aws s3 sync $AWS_S3_LOCAL_PATH $DOCS_BUCKET --delete --acl public-read --region $DOCS_REGION
