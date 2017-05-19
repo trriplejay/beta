6c1bb180021da87efe4d168731e0cbe5307dd816
Devashish <devashish.86@gmail.com>
2017-05-19 16:45:17 -0700

    updating command

--- deployRc.sh
+++ deployRc.sh
@@ -93,12 +93,10 @@ deploy() {
   echo "-------------------------------------="
 
   #local deploy_command="sudo /home/ubuntu/base/base.sh upgrade --version $DEPLOY_VERSION"
-  pushd /home/ubuntu/admiral
-  local deploy_command="sudo ./admiral.sh upgrade"
+  local deploy_command="cd /home/ubuntu/admiral && sudo ./admiral.sh upgrade"
   echo "Executing deploy command: $deploy_command"
   ssh -A $BASTION_USER@$BASTION_IP ssh $SWARM_USER@$SWARM_IP "$deploy_command"
   echo "-------------------------------------="
-  popd
 
   echo "Successfully deployed release $DEPLOY_VERSION to rc env"
 }
